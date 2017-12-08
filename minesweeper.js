e12f8f857db402fb2ee0fdaa11faf3482add6089
Julia Mareike <jnmareike@gmail.com>
2017-12-08 15:21:21 +1300

    add missing row

--- minesweeper.js
+++ minesweeper.js
@@ -5,9 +5,12 @@ var board = {
   cells: [{ row: 0, col: 0, isMine: false, hidden: true },
           { row: 0, col: 1, isMine: false, hidden: true },
           { row: 0, col: 2, isMine: false, hidden: true },
-          { row: 1, col: 0, isMine: true, hidden: true },
+          { row: 1, col: 0, isMine: false, hidden: true },
           { row: 1, col: 1, isMine: true, hidden: true },
-          { row: 1, col: 2, isMine: true, hidden: true }]
+          { row: 1, col: 2, isMine: true, hidden: true },
+          { row: 2, col: 0, isMine: true, hidden: true },
+          { row: 2, col: 1, isMine: false, hidden: true },
+          { row: 2, col: 2, isMine: true, hidden: true }]
 }
 
 function startGame() {
